8155762b9269eafe61d711050089b7c9d6756cef
Kris <kristong222@qq.com>
2019-07-24 23:17:29 +0800

    书写了注册函数

--- DjanoGit/index.py
+++ DjanoGit/index.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     print('python test')
     return HttpResponse('Hello')
+
+def register(requests):
+    print('这是注册函数')
+    return HttpResponse('注册成功')
