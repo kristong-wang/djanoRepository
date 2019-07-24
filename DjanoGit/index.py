b5f8a6a673c6c78ead6cedefb1dbde04dcd76354
Kris <kristong222@qq.com>
2019-07-24 23:11:08 +0800

    添加了dev

--- DjanoGit/index.py
+++ DjanoGit/index.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     print('python test')
     return HttpResponse('Hello')
+
+def login(request):
+    print('这是登录')
+    return HttpResponse()
